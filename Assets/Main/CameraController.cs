9deebf9044f2b56ac1fad3b53cebea2b9b82e3fa
rimayu0815 <rikussyuu@gmail.com>
2021-03-06 15:33:13 +0900

    最初のコミット

---
+++ Assets/Main/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 offset;
+
+    [SerializeField]
+    private GameObject unitychan;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        unitychan = GameObject.Find("unitychan");
+
+        offset = transform.position - unitychan.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(unitychan != null)
+        {
+            transform.position = unitychan.transform.position + offset;
+        }
+    }
+}
